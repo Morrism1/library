d5587c7836b029d2032bea8dd7486e59d75d953f
Morrism1 <morrismontana0@gmail.com>
2021-01-13 16:45:44 +0200

    Add modal and display array elements in a table

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-let myLibrary = ["js","css", "java", "ruby"];
-let addbutton= document.querySelector("button");
+let myLibrary = ["js", "css", "java", "ruby"];
+let addbutton = document.querySelector("button");
 addbutton.addEventListener("click", addBookToLibrary);
 
+// get the table
+
+const table = document.querySelector("tr");
 
 function Book() {
   // the constructor...
-
-
 }
 
 function addBookToLibrary() {
@@ -16,14 +17,29 @@ function addBookToLibrary() {
   console.log(myLibrary);
 }
 
- function displayBook()
- { 
-for (let books in myLibrary)
-{ 
-    console.log(myLibrary[books]);
+function displayBook() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let cell = document.createElement("td");
+    let cellText = document.createTextNode(myLibrary[i]);
+    cell.appendChild(cellText);
+    table.appendChild(cell);
+  }
 }
- }
 
+displayBook();
 
+// create modal
+// 1. get all elements
+const modal = document.querySelector("#myModal");
+const modelContent = document.querySelector(".modal-content");
+const modalBtn = document.querySelector("#myBtn");
+const closeModal = document.querySelector(".close");
 
+// open the modal
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
 
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
