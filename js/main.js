ea4ef8048561613d80b7dd3cbbf8dc8790723606
Morrism1 <morrismontana0@gmail.com>
2021-01-18 11:39:57 +0200

    Add card to the dom and delete card button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-let myLibrary = ["js", "css", "java", "ruby"];
+let myLibrary = [
+  {
+    title: "Been through Hell",
+    author: "John Smith",
+    pages: 220,
+    read: "Sure Soon",
+  },
+  {
+    title: "Good life",
+    author: "Jane Smith",
+    pages: 120,
+    read: "Well Well",
+  },
+];
+
+const heroPlaceholder = document.querySelector("#hero-template");
 let addbutton = document.querySelector("button");
 addbutton.addEventListener("click", addBookToLibrary);
 
@@ -18,12 +33,26 @@ function addBookToLibrary() {
 }
 
 function displayBook() {
+  const list = document.createElement("ul");
+  list.classList.add("list", "hero-list");
+
+  const template = document.querySelector("#hero-template");
+
   for (let i = 0; i < myLibrary.length; i++) {
-    let cell = document.createElement("td");
-    let cellText = document.createTextNode(myLibrary[i]);
-    cell.appendChild(cellText);
-    table.appendChild(cell);
+    const heroCard = document.importNode(template.content, true);
+    const removeButton = heroCard.querySelector("button");
+    heroCard.querySelector("li").setAttribute("data-num", i);
+    heroCard.querySelector(".card-title").textContent = myLibrary[i].title;
+    heroCard.querySelector(".card-text").textContent = myLibrary[i].author;
+    removeButton.textContent = "Remove book";
+    removeButton.addEventListener("click", () => {
+      document.querySelector(`[data-num="${i}"]`).remove();
+    });
+    console.log(myLibrary[i].author);
+    list.appendChild(heroCard);
   }
+
+  heroPlaceholder.replaceWith(list);
 }
 
 displayBook();
@@ -43,3 +72,9 @@ modalBtn.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
+
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+};
